972dfd3bf2ce138ac7860d6a863ee692cd0352fe
narakuhub <narakuhub@gmail.com>
2026-07-04 18:27:42 +0700

    Update BetaV1.2.js

--- api/BetaV1.2.js
+++ api/BetaV1.2.js
@@ -143,6 +143,6 @@ export default async function handler(req, res) {
 `;
 
     res.setHeader('Content-Type', 'text/html');
-    return res.status(403).send(htmlResponse);
+    return res.status(200).send(htmlResponse);
   }
 }
